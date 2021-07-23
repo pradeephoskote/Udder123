6d4880cb37d88e42bb053b5a674c328a2d0d0ad6
Pradeep Hoskote <hoskote.pradeep@gmail.com>
2021-07-23 08:52:04 +0530

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Pradeep kumar Hoskote on 23/07/21.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
